99c4cc88e0bdb1d2b0fccdf842eccc4ff66ebbad
kanziw <kanziwoong@gmail.com>
2020-02-23 17:16:34 +0900

    Add testcase

--- index.js
+++ index.js
@@ -3,20 +3,23 @@ const snakeCase = require('lodash.snakecase')
 const isString = value => typeof value === 'string'
 const isNumber = value => typeof value === 'number' && !isNaN(value)
 const isArray = value => Array.isArray(value)
+const isBoolean = value => typeof value === 'boolean'
 
 const snakify = obj => {
   if (isArray(obj)) {
     return obj.map(item => snakify(item))
   }
 
-  if (!obj || isString(obj) || isNumber(obj)) {
+  if (!obj || isString(obj) || isNumber(obj) || isBoolean(obj)) {
     return obj
   }
 
   return Object
     .entries(obj)
     .reduceRight(
-      (acc, [ key, value ]) => Object.assign({ [ snakeCase(key) ]: value }, acc),
+      (acc, [ key, value ]) => Object.assign({ [ snakeCase(key) ]: snakify(value) }, acc),
       {},
     )
 }
+
+module.exports = snakify
---
+++ snakify.test.js
@@ -0,0 +1,41 @@
+const snakify = require('./index')
+
+test('shallow depth', () => {
+  const given = { shallowDepth: true }
+  const expected = { shallow_depth: true }
+
+  expect(snakify(given)).toEqual(expected)
+})
+
+test('deep depth', () => {
+  const given = { oneDepth: { twoDepth: { threeDepth: true }, twoDepth2: { threeDepth2: false } } }
+  const expected = { one_depth: { two_depth: { three_depth: true }, two_depth_2: { three_depth_2: false } } }
+
+  expect(snakify(given)).toEqual(expected)
+})
+
+test('various types', () => {
+  const given = { numVal: 1, strVal: 'camelCase', boolVal: true }
+  const expected = { num_val: 1, str_val: 'camelCase', bool_val: true }
+
+  expect(snakify(given)).toEqual(expected)
+})
+
+test('array case', () => {
+  const given = {
+    arrVal: [ 1, 'camelCaseArrVal', true ],
+    arrObj: [
+      { numVal: 1, strVal: 'camelCase', boolVal: true },
+      { numVal: 1, strVal: 'camelCase', boolVal: true },
+    ],
+  }
+  const expected = {
+    arr_val: [ 1, 'camelCaseArrVal', true ],
+    arr_obj: [
+      { num_val: 1, str_val: 'camelCase', bool_val: true },
+      { num_val: 1, str_val: 'camelCase', bool_val: true },
+    ],
+  }
+
+  expect(snakify(given)).toEqual(expected)
+})
